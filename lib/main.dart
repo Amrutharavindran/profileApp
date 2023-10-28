c308f835a3c48abe8108d7bf6a1d8cecfd78067a
Amrutharavindran <amrutha24916@gmail.com>
2023-10-28 15:48:11 +0530

    initial message

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MaterialApp(home: Center(child: Text("homepage",style:TextStyle( decoration:TextDecoration.none ,fontSize: 30,color: Colors.brown,)),)));
+}
